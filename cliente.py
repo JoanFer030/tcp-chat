a03e632ded8af493b5c8ccabe4b5c76d8f99ae04
Joan Fernández Navarro <joanfernandeznv@gmail.com>
2022-10-24 23:44:18 +0200

    version4-encriptado

--- cliente.py
+++ cliente.py
@@ -2,17 +2,38 @@ from socket import *
 import os
 from threading import Thread
 from time import sleep
+from cryptography.fernet import Fernet
 
 class ClientChat:
     def __init__(self, server_name, server_port):
         self.cs = socket(AF_INET, SOCK_STREAM)
         self.cs.connect((server_name, server_port))
         self.stop = True
+        self.fernet = None
+
+    def decrypt_msg(self,enc_msg):
+        msg = self.fernet.decrypt(enc_msg).decode()
+        return msg
+
+    def encrypt_msg(self,msg):
+        enc_msg = self.fernet.encrypt(msg.encode())
+        return enc_msg
+
+    def set_key(self):
+        message = self.cs.recv(1024)
+        if message.decode().startswith("/key"):
+            key = message.decode().replace("/key", "").encode()
+            self.fernet = Fernet(key)
+            self.cs.send(self.encrypt_msg("key-ok"))
+            message = self.decrypt_msg(self.cs.recv(1024))
+            if message != "ok":
+                print("Clave de encriptado no recibida correctamente.")
         
     def set_nick(self):
         prov_nick = "/nick" + input("Introduzca nombre de usuario: ")
-        self.cs.send(prov_nick.encode())
-        server_ans = self.cs.recv(1024).decode().split("-")
+        self.cs.send(self.encrypt_msg(prov_nick))
+        server_ans = self.cs.recv(1024)
+        server_ans = self.decrypt_msg(server_ans).split("-")
         if server_ans[0] == "OK":
             os.system("cls||clear")
             print(server_ans[1])
@@ -28,11 +49,12 @@ class ClientChat:
                 self.exit()
                 self.stop = False
                 break
-            self.cs.send(msg.encode())
+            self.cs.send(self.encrypt_msg(msg))
 
     def receive_messages(self):
         while self.stop:
-            message = self.cs.recv(1024).decode()
+            message = self.cs.recv(1024)
+            message = self.decrypt_msg(message)
             if message == "/kick":
                 self.exit()
                 self.stop = False
@@ -40,25 +62,22 @@ class ClientChat:
             print(message)
 
     def exit(self):
-        self.cs.send("/exit".encode())
+        self.cs.send(self.encrypt_msg("/exit"))
         print("Saliste del Chat")
 
 
-while True:
-    try:
-        os.system("cls||clear")
-        ip = input("Introducir IP del chat: ")
-        port = int(input("Introducir puerto del chat: "))
-        chat = ClientChat(ip, port)
-        print("Entrando al chat...")
-        sleep(1)
-        chat.set_nick()
-        break
-    except:
-        print("IP o puerto erroneos.")
-        sleep(3)
+
+os.system("cls||clear")
+ip = input("Introducir IP del chat: ")
+port = int(input("Introducir puerto del chat: "))
+chat = ClientChat(ip, port)
+print("Entrando al chat...")
+chat.set_key()
+sleep(1)
+chat.set_nick()
 
 receive_thread = Thread(target=chat.receive_messages)
 receive_thread.start()
 send_thread = Thread(target=chat.send_message)
 send_thread.start()
+
--- servidor.py
+++ servidor.py
@@ -1,6 +1,7 @@
 from socket import *
 from threading import Thread
 import os
+from cryptography.fernet import Fernet
 
 class ServerChat:
     def __init__(self, server_ip="", server_port=12000):
@@ -15,30 +16,48 @@ class ServerChat:
         self.server_socket = socket(AF_INET,SOCK_STREAM)
         self.server_socket.bind((server_ip, server_port))
         self.server_socket.listen()
+        self.key = Fernet.generate_key()
+        self.fernet = Fernet(self.key)
+
+    def decrypt_msg(self,enc_msg):
+        msg = self.fernet.decrypt(enc_msg).decode()
+        return msg
+
+    def encrypt_msg(self,msg):
+        enc_msg = self.fernet.encrypt(msg.encode())
+        return enc_msg
+
+    def send_key(self, cs):
+        key = "/key" + self.key.decode()
+        cs.send(key.encode())
+        msg = self.decrypt_msg(cs.recv(1024))
+        if msg == "key-ok":
+            cs.send(self.encrypt_msg("ok"))
 
     def send_to_all(self, msg, cs=""):
         for client in self.users:
             if client != cs:
-                client.send(msg.encode())
+                client.send(self.encrypt_msg(msg))
 
     def welcome(self):
         while True:
             connection_socket, address = self.server_socket.accept()
+            self.send_key(connection_socket)
             listen_thread = Thread(target=self.listen, args=(connection_socket, address))
             listen_thread.start()
 
     def listen(self, connection_socket, address):
         while True:
             try:
-                msg = connection_socket.recv(1024).decode()
+                msg = self.decrypt_msg(connection_socket.recv(1024))
                 if msg.startswith("/nick") and msg[5:] not in self.users.values():
                     nick = msg[5:]
                     self.users[connection_socket] = (nick, address)
-                    connection_socket.send(f"OK-Bienvenido a {self.chat_name}".encode())
+                    connection_socket.send(self.encrypt_msg(f"OK-Bienvenido a {self.chat_name}"))
                     self.send_to_all(f"server> Nuevo usuario '{nick}'", connection_socket)
                     print(f"server> Nuevo usuario con dirección: {address[0]} y nick: {nick}")
                 elif msg.startswith("/nick") and msg[5:] in self.users.values():
-                    connection_socket.send("NO".encode())
+                    connection_socket.send(self.encrypt_msg("NO"))
                 elif msg == "/exit":
                     msg = f"> {self.users[connection_socket]} abandono el chat"
                     print(msg)
@@ -77,7 +96,7 @@ class ServerChat:
                 for client, info in self.users.items():
                     if nick == info[0]:
                         exists = True
-                        client.send("/kick".encode())
+                        client.send(self.encrypt_msg("/kick"))
                 if exists:
                     print(f"{nick} ha sido expulsado")
                 else:
